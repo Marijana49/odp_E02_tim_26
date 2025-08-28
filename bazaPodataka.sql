793b09e5ddba144952b1da5c673771cf1b51c7f4
Marijana49 <marijanazaric8@gmail.com>
2025-08-28 09:35:04 +0200

    baza popravljena

--- bazaPodataka.sql
+++ bazaPodataka.sql
@@ -15,7 +15,8 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS messages (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    korisnickoIme VARCHAR(50) NOT NULL UNIQUE,
+    korisnickoIme VARCHAR(50) NOT NULL,
+    ulogovani varchar(50),
     primljenePoruke VARCHAR(1000),
     poslatePoruke VARCHAR(1000),
     stanjePoruke VARCHAR(10),
